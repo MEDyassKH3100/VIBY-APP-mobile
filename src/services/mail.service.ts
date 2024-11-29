4dfdf189623dc21536948c95a54d4323daeaa2af
Mohamed Yassine Khlaf <150388066+MEDyassKH3100@users.noreply.github.com>
2024-11-29 11:46:26 +0100

    add backend

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -9,7 +9,9 @@ export class MailService {
   constructor() {
     // Set up transporter to use Gmail
     this.transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      host: 'smtp.gmail.com', // ou utilisez un autre service SMTP
+      port: 587,
+      secure: false, // utilisez true pour le port 465
       auth: {
         user: 'chatesprit3@gmail.com',
         pass: 'vdhu sqjt wuid vjkr',
@@ -23,15 +25,19 @@ export class MailService {
        <h1>Email Verification</h1>
     <p>Please click the button below to verify your email:</p>
     <a href="${verificationUrl}" style="background-color: #4CAF50; color: white; padding: 14px 25px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px;">Verify Email</a>
-  `;
-
+   `;
+   try {
     await this.transporter.sendMail({
       from: 'Auth-backend service <chatesprit3@gmail.com>',
       to: email,
       subject: 'Verify Your Email',
-      html: html
+      html: html,
     });
+  } catch (error) {
+    console.error('Error sending email:', error.message);
+    throw new Error('Email sending failed');
   }
+}
 
   async sendOtpEmail(to: string, otp: number) {
     const htmlTemplate = `
